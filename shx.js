931daff28cb8bc6c54c068d5d0c5166687461619
孙昊翔 <673686754@qq.com>
2017-01-09 17:43:10 +0800

    2017.1.9

---
+++ shx.js
@@ -0,0 +1,27 @@
+
+function doMove ( obj, attr, dir, target, endFn ) {
+	
+	dir = parseInt(getStyle( obj, attr )) < target ? dir : -dir;
+	
+	clearInterval( obj.timer );
+	
+	obj.timer = setInterval(function () {
+		
+		var speed = parseInt(getStyle( obj, attr )) + dir;
+		
+		if ( speed > target && dir > 0 ||  speed < target && dir < 0  ) {
+			speed = target;
+		}
+		
+		obj.style[attr] = speed + 'px';
+		
+		if ( speed == target ) {
+			clearInterval( obj.timer );
+			endFn && endFn();
+			
+		}
+		
+	}, 30);
+}
+
+function getStyle ( obj, attr ) { return obj.currentStyle?obj.currentStyle[attr] : getComputedStyle( obj )[attr]; }
