c67c9bc647aaba921d1bc13befea38ab2156c473
孙昊翔 <673686754@qq.com>
2017-01-10 07:47:42 +0800

    2017.1.10

--- shx.js
+++ shx.js
@@ -1,4 +1,3 @@
-
 function doMove ( obj, attr, dir, target, endFn ) {
 	
 	dir = parseInt(getStyle( obj, attr )) < target ? dir : -dir;
@@ -6,7 +5,6 @@ function doMove ( obj, attr, dir, target, endFn ) {
 	clearInterval( obj.timer );
 	
 	obj.timer = setInterval(function () {
-		
 		var speed = parseInt(getStyle( obj, attr )) + dir;
 		
 		if ( speed > target && dir > 0 ||  speed < target && dir < 0  ) {
@@ -18,10 +16,36 @@ function doMove ( obj, attr, dir, target, endFn ) {
 		if ( speed == target ) {
 			clearInterval( obj.timer );
 			endFn && endFn();
-			
 		}
 		
 	}, 30);
+}
+
+function shake(obj,attr) {
+    if(obj.onOff) return;
+    obj.onOff = true;
+    var pos = parseInt(getStyle(obj,attr));
+    var arr = [];
+    var num = 0;
+    var timer = null;
+    for(var i=20;i>0;i-=2){
+        arr.push(i,-i);
+    }
+    arr.push(0);
+
+    clearInterval(obj.shake);
+    obj.shake = setInterval(function () {
+        obj.style[attr] = pos + arr[num] + 'px';
+        num++;
+        if(num===arr.length){
+            clearInterval(obj.shake);
+            obj.onOff = false;
+        }
+    },50)
+}
+
+function toTwo ( n ) {
+    return n < 10 ?  '0' + n : '' + n;
 }
 
 function getStyle ( obj, attr ) { return obj.currentStyle?obj.currentStyle[attr] : getComputedStyle( obj )[attr]; }
