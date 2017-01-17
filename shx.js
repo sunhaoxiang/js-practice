63dbad4e9654bf7558b09f350b1b5a17211abe8b
孙昊翔 <673686754@qq.com>
2017-01-17 19:47:33 +0800

    2017.1.17

--- shx.js
+++ shx.js
@@ -1,24 +1,17 @@
 //运动
 function doMove ( obj, attr, dir, target, endFn ) {
-	
 	dir = parseInt(getStyle( obj, attr )) < target ? dir : -dir;
-	
 	clearInterval( obj.timer );
-	
 	obj.timer = setInterval(function () {
 		var speed = parseInt(getStyle( obj, attr )) + dir;
-		
 		if ( speed > target && dir > 0 ||  speed < target && dir < 0  ) {
 			speed = target;
 		}
-		
 		obj.style[attr] = speed + 'px';
-		
 		if ( speed == target ) {
 			clearInterval( obj.timer );
 			endFn && endFn();
 		}
-		
 	}, 30);
 }
 
@@ -34,7 +27,6 @@ function shake(obj,attr) {
         arr.push(i,-i);
     }
     arr.push(0);
-
     clearInterval(obj.shake);
     obj.shake = setInterval(function () {
         obj.style[attr] = pos + arr[num] + 'px';
@@ -62,5 +54,56 @@ function getPos(obj) {
     return pos;
 }
 
+//获取style
 function getStyle ( obj, attr ) { return obj.currentStyle?obj.currentStyle[attr] : getComputedStyle( obj )[attr]; }
 
+//获取class
+function getElementsByClassName(oParent, tagName, className) {
+    var aEls = oParent.getElementsByTagName(tagName);
+    var arr = [];
+    for (var i=0; i<aEls.length; i++) {
+        var arrClassName = aEls[i].className.split(' ');
+        for (var j=0; j<arrClassName.length; j++) {
+            if ( arrClassName[j] == className ) {
+                arr.push(aEls[i]);
+                break;
+            }
+        }
+    }
+    return arr;
+}
+
+//添加class
+function addClass(obj, className) {
+    if (obj.className == '') {
+        obj.className = className;
+    } else {
+        var arrClassName = obj.className.split(' ');
+        if ( arrIndexOf(arrClassName, className) == -1 ) {
+            obj.className += ' ' + className;
+            /*arrClassName.push(className);
+             obj.className = arrClassName.join(' ');*/
+        }
+    }
+}
+
+//移除class
+function removeClass(obj, className) {
+    if (obj.className != '') {
+        var arrClassName = obj.className.split(' ');
+        var _index = arrIndexOf(arrClassName, className);
+        if ( _index != -1 ) {
+            arrClassName.splice(_index, 1);
+            obj.className = arrClassName.join(' ');
+        }
+    }
+}
+
+function arrIndexOf(arr, v) {
+    for (var i=0; i<arr.length; i++) {
+        if (arr[i] == v) {
+            return i;
+        }
+    }
+    return -1;
+}
