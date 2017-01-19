38ccbaf8242a05b39f8c8663d7f74ecc7bbd1c03
孙昊翔 <673686754@qq.com>
2017-01-19 18:19:44 +0800

    2017.1.19

--- shx.js
+++ shx.js
@@ -118,3 +118,40 @@ function bind(obj,evname,fn) {
         });
     }
 }
+
+//拖拽
+function drag(obj) {
+    obj.onmousedown = function(ev) {
+        var ev = ev || event;
+        var disX = ev.clientX - this.offsetLeft;
+        var disY = ev.clientY - this.offsetTop;
+
+        if ( obj.setCapture ) {
+            obj.setCapture();
+        }
+        document.onmousemove = function(ev) {
+            var ev = ev || event;
+            var L = ev.clientX -disX;
+            var T = ev.clientY -disY;
+            if (L<0) {
+                L = 0;
+            } else if (L>document.documentElement.clientWidth-obj.offsetWidth) {
+                L = document.documentElement.clientWidth-obj.offsetWidth;
+            }
+            if (T<0) {
+                T = 0;
+            } else if (T>document.documentElement.clientHeight-obj.offsetHeight) {
+                T = document.documentElement.clientHeight-obj.offsetHeight;
+            }
+            obj.style.left = L + 'px';
+            obj.style.top = T + 'px';
+        };
+        document.onmouseup = function() {
+            document.onmousemove = document.onmouseup = null;
+            if ( obj.releaseCapture ) {
+                obj.releaseCapture();
+            }
+        };
+        return false;
+    }
+}
