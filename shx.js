15cebc83c74f3157d8dd4717060e4c93474a0a0e
孙昊翔 <673686754@qq.com>
2017-01-19 21:48:49 +0800

    2017.1.19

--- shx.js
+++ shx.js
@@ -155,3 +155,26 @@ function drag(obj) {
         return false;
     }
 }
+
+//设置cookie
+function setCookie(key, value, t) {
+    var oDate = new Date();
+    oDate.setDate( oDate.getDate() + t );
+    document.cookie = key + '=' + value + ';expires=' + oDate.toGMTString();
+}
+
+//读取cookie
+function getCookie(key) {
+    var arr1 = document.cookie.split('; ');
+    for (var i = 0; i < arr1.length; i++) {
+        var arr2 = arr1[i].split('=');
+        if (arr2[0]==key) {
+            return decodeURI(arr2[1]);
+        }
+    }
+}
+
+//删除cookie
+function removeCookie(key) {
+    setCookie(key,'',-1);
+}
