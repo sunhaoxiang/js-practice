fd64fab05e81b40e1f4bddb80f2705afaec73db6
孙昊翔 <673686754@qq.com>
2017-01-23 23:28:22 +0800

    2017.1.23

---
+++ jQueryAjax/data.php
@@ -0,0 +1,13 @@
+<?php
+
+if($_GET['num']==0){
+	echo 'Ajax加载的第一项的内容';
+}
+else if($_GET['num']==1){
+	echo 'Ajax加载的第二项的内容';
+}
+else if($_GET['num']==2){
+	echo 'Ajax加载的第三项的内容';
+}
+
+?> 
---
+++ jQueryAjax/user.php
@@ -0,0 +1,10 @@
+<?PHP
+
+	if($_GET['user'] == 'sunhaoxiang'){
+		echo '0';
+	}
+	else{
+		echo '1';
+	}
+
+?>
